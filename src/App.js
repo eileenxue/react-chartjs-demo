6cf7af28a34ae1cbbc155b63a5fe0c7d4b75f76e
Eileen Xue <xue.eileen@gmail.com>
2022-06-03 01:52:21 -0400

    Create bar, line, and pie chart demo with fake data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
+import { UserData } from "./Data"
 
 function App() {
+
+  const [userGainData, setUserGainData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [{
+      label: "Users Gained", 
+      data: UserData.map((data) => data.userGain),
+      backgroundColor: ["#21897E", "#3BA99C", "#69D1C5", "#7EBCE6", "#8980F5"]
+    }]
+  });
+
+  const [userLostData, setUserLostData] = useState({
+    labels: UserData.map((data) => data.year),
+    datasets: [{
+      label: "Users Lost", 
+      data: UserData.map((data) => data.userLost),
+    }]
+  });
+
   return (
     <div className="App">
-      
+      <div style={{width: 500}}>
+        <BarChart chartData={userGainData}/>
+        <LineChart chartData={userLostData}/>
+        <PieChart chartData={userGainData}/>
+      </div>
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,33 @@
+// Fake data
+export const UserData = [
+  {
+    id: 1, 
+    year: 2015,
+    userGain: 80000,
+    userLost: 723,
+  },
+  {
+    id: 2, 
+    year: 2016,
+    userGain: 100000,
+    userLost: 1723,
+  },
+  {
+    id: 3, 
+    year: 2017,
+    userGain: 58000,
+    userLost: 489,
+  },
+  {
+    id: 4, 
+    year: 2018,
+    userGain: 96000,
+    userLost: 1573,
+  },
+  {
+    id: 5, 
+    year: 2019,
+    userGain: 120000,
+    userLost: 2023,
+  },
+]
